6ee3e62935f4e3213be84dd5163dc86fd615d878
Yey <prakt3@list.ru>
2019-06-14 16:52:56 +0500

    First commit

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>Страница для абитуриентов</title>
+
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+  </head>
+  <body>
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+
+			<div class="row">
+				<div class="col-md-12">
+					<h1>ШАПКА</h1>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<h2>ХОД ПРИЁМа</h2>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-4">
+
+				</div>
+				<div class="col-md-4">
+
+				</div>
+				<div class="col-md-4">
+
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-3">
+
+				</div>
+				<div class="col-md-3">
+
+				</div>
+				<div class="col-md-3">
+
+				</div>
+				<div class="col-md-3">
+
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+  </body>
+</html>
