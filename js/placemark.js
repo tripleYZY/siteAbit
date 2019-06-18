5819e62f1030d650875a2d8c40f0e1ea940b0c78
Yey <prakt3@list.ru>
2019-06-18 16:46:20 +0500

    Signed-off-by: Yey <prakt3@list.ru>

---
+++ js/placemark.js
@@ -0,0 +1,20 @@
+ymaps.ready(init);
+
+function init() {
+    var myMap = new ymaps.Map("map", {
+            center: [54.7232153, 55.9483825],
+            zoom: 17
+        }, {
+            searchControlProvider: 'yandex#search'
+        });
+
+    myMap.geoObjects
+        .add(new ymaps.Placemark([54.7232153, 55.9483825], {
+            balloonContentHeader: 'Приемная комиссия',
+            balloonContentBody: "<span>Уфа, Октябрьской Революции, 3а <br> <br> +7 (347) 272-11-16 <br> bspu.pk@mail.ru <br><br> </span>",
+            balloonContentFooter: "График работы: 9:00 - 17:00 (пн-пт)"
+        }, {
+            preset: 'islands#icon',
+            iconColor: '#183884'
+        }));
+}
