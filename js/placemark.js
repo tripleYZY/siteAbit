c01e613d7bc846c24ce730031dc3be1b72ff81c9
Yey <prakt3@list.ru>
2019-06-19 16:38:39 +0500

    BACKUP

--- js/placemark.js
+++ js/placemark.js
@@ -11,7 +11,7 @@ function init() {
     myMap.geoObjects
         .add(new ymaps.Placemark([54.7232153, 55.9483825], {
             balloonContentHeader: 'Приемная комиссия',
-            balloonContentBody: "<span>Уфа, Октябрьской Революции, 3а <br> <br> +7 (347) 272-11-16 <br> bspu.pk@mail.ru <br><br> </span>",
+            balloonContentBody: "<span>Уфа, Октябрьской Революции, 3а <br> +7 (347) 272-11-16 <br> bspu.pk@mail.ru <br><br> </span>",
             balloonContentFooter: "График работы: 9:00 - 17:00 (пн-пт)"
         }, {
             preset: 'islands#icon',
